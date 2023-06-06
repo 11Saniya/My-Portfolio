8c8b7ad66ae2631a4254bfab5b16a3db022c066a
Saniya <saniyaanjum142115@gmail.com>
2023-06-06 23:57:48 +0530

    update all

---
+++ resources/index.js
@@ -0,0 +1,10 @@
+const about = document.getElementById("mail");
+onclick.addEventListener('click',mail);
+
+const github = document.getElementById("git");
+onclick.addEventListener('click',github);
+
+const linked = document.getElementById("linkedin");
+onclick.addEventListener('click',linked);
+
+
