df1fee21780c4e1ca10e848b19fd293c0ac1d24c
Saniya <saniyaanjum142115@gmail.com>
2023-06-14 12:16:33 +0530

    upadate all files

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,4 @@
+
 const about = document.getElementById("mail");
 onclick.addEventListener('click',mail);
 
@@ -6,7 +7,5 @@ onclick.addEventListener('click',github);
 
 const linked = document.getElementById("linkedin");
 onclick.addEventListener('click',linked);
-
-
 
 
